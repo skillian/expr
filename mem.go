94c5f2da6251013ab05a66e3fa5bd5bccf26b554
Sean Killian <skillian92@gmail.com>
2021-06-06 08:54:52 -0400

    Added caching of member access via MemOf

--- mem.go
+++ mem.go
@@ -2,6 +2,7 @@ package expr
 
 import (
 	"reflect"
+	"sync"
 
 	"github.com/skillian/expr/errors"
 )
@@ -11,6 +12,10 @@ type Mem [2]Expr
 
 // Operands implements the Binary interface.
 func (e Mem) Operands() [2]Expr { return [2]Expr(e) }
+
+// memberCache maps (reflect.Type, offset int64) to a reflect.Type's Field
+// index.
+var memberCache sync.Map
 
 // MemOf is a clunky way to work around Go's lack of having an expression type
 // like C# has.  Source is the actual source expression that the the member
@@ -38,16 +43,28 @@ func MemOf(source, struc, field interface{}) Mem {
 	if rf.Kind() != reflect.Ptr {
 		panic(errors.Errorf1("%v must be a pointer to a field of struc", field))
 	}
-	offset := rf.Pointer() - rv.Pointer()
-	rt := rv.Elem().Type()
-	if rt.Kind() != reflect.Struct {
-		panic(errors.Errorf2("%v must be a struct, not %v", struc, rt))
+	type memberCacheKey struct {
+		reflect.Type
+		Offset uintptr
 	}
-	nf := rt.NumField()
+	mck := memberCacheKey{
+		Type: rv.Elem().Type(),
+		Offset: rf.Pointer() - rv.Pointer(),
+	}
+	if mck.Type.Kind() != reflect.Struct {
+		panic(errors.Errorf2("%v must be a struct, not %v", struc, mck.Type))
+	}
+	var key interface{} = mck
+	x, ok := memberCache.Load(key)
+	if ok {
+		return Mem{source, x}
+	}
+	nf := mck.Type.NumField()
 	for i := 0; i < nf; i++ {
-		sf := rt.Field(i)
-		if sf.Offset == offset {
-			return Mem{source, i}
+		sf := mck.Type.Field(i)
+		if sf.Offset == mck.Offset {
+			x, _ = memberCache.LoadOrStore(key, i)
+			return Mem{source, x}
 		}
 	}
 	panic(errors.Errorf2("unable to find field %v of %v", field, struc))
